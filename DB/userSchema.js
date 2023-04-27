a7ab7e97670619ab4eb7919864941e6787430831
Zamza584 <mzamalloa97@gmail.com>
2023-04-27 14:37:20 -0500

    new commit

--- DB/userSchema.js
+++ DB/userSchema.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-  },
+  firstName: String,
+  lastName: String,
 });
 
 module.exports = mongoose.model("user", userSchema);
