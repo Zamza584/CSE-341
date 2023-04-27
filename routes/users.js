7854ed0de53367ae4096f6f4897182b8278bc57a
Zamza584 <mzamalloa97@gmail.com>
2023-04-27 12:55:37 -0500

    retrieves data from mongodb

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const userSchema = require("../DB/userSchema");
+const router = require("express").Router();
+
+router.post("/", async (req, res) => {
+  const { firstName, lastName } = req.body;
+  let user = {};
+  user.firstName = firstName;
+  user.lastName = lastName;
+  let userModel = new userSchema(user);
+  try {
+    await userModel.save();
+    res.json(userModel);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+//get all users
+router.get("/", async (req, res) => {
+  try {
+    const allUsers = await userSchema.find()
+    res.json(allUsers);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000
+const connectDb = require("./DB/mongodb");
+connectDb();
 
-app.use('/', require("./routes"))
+const port = process.env.PORT || 8000;
+
+app.use(express.json({ extended: false }));
+
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
 app.listen(port, () => {
   console.log("Server started at http://localhost:" + port);
