5eba46a588baf1ed3491d7f5bdaf9811f5f13327
Zamza584 <mzamalloa97@gmail.com>
2023-05-13 14:37:06 -0500

    swagger 2

--- utils/swagger.js
+++ utils/swagger.js
@@ -1,14 +1,14 @@
-const options = { 
-  openapi: '3.0.0',
-  autoBody:  true      
-}
+const options = {
+  openapi: "3.0.0",
+  autoBody: true
+};
 
 const swaggerAutogen = require("swagger-autogen")(options);
 
 const doc = {
   info: {
-    title: "My API",
-    description: "Description"
+    title: "My API CSE-341",
+    description: "Api used for our CSE 341 class"
   },
   servers: [
     {
@@ -16,31 +16,33 @@ const doc = {
       description: "local server"
     }
   ],
-  host: "cse-341-yf6e.onrender.com",  
-  basePath: '/',
+  host: "cse-341-yf6e.onrender.com",
+  basePath: "/",
   schema: ["https"],
 
   definitions: {
-    contacts: { 
-      type: "object",
-      properties: { 
-        title: { 
-          _id: {
-            type: "String",
-          }
-        }
-      }
+    contacts: {
+      firstName: "<String>",
+      lastName: "<String>",
+      email: "<String>",
+      favoriteColor: "<String>",
+      birthday: "<String>"
+    },
+    updateContacts: {
+      firstName: "<String>",
+      lastName: "<String>",
+      email: "<String>",
+      favoriteColor: "<String>",
+      birthday: "<String>"
 
     }
   }
-
 };
 
 const outputFile = "./utils/swagger-output.json";
 const endpointsFiles = ["./server.js"];
 
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-  require('../server.js'); // Your project's root file
-  console.log("api doc found at: https://cse-341-yf6e.onrender.com/api-docs/")
+  require("../server.js"); // Your project's root file
+  console.log("api doc found at: https://cse-341-yf6e.onrender.com/api-docs/");
 });
-    
